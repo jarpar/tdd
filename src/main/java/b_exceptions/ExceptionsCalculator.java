b724ba6b9fff9f4b8a3275f290378dccbe4854c3
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-08-22 12:22:49 +0200

    refactored

---
+++ src/main/java/b_exceptions/ExceptionsCalculator.java
@@ -0,0 +1,15 @@
+package b_exceptions;
+
+public class ExceptionsCalculator {
+
+    Integer divide(Integer a, Integer b) {
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("NULL ARGUMENT");
+        }
+
+        if (b == 0) {
+            throw new IllegalArgumentException("DIVIDE BY 0");
+        }
+        return a / b;
+    }
+}
