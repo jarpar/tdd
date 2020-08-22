a8b78f64500fdb33a0a0de31d03b70ce8f6a2510
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-08-22 11:38:58 +0200

    refactored

--- src/test/java/a_junit/CalculatorClass.java
+++ src/test/java/a_junit/CalculatorClass.java
@@ -2,9 +2,9 @@ package a_junit;
 
 import org.junit.jupiter.api.*;
 
-import java.time.LocalTime;
-
 public class CalculatorClass {
+    Calculator calculator;
+
     @BeforeAll
     public static void beforeAll() {
         System.out.println("BEFORE ALL");
@@ -15,26 +15,20 @@ public class CalculatorClass {
         System.out.println("AFTER ALL");
     }
 
-    Integer time;
-
     @BeforeEach
     public void beforeEach() {
         System.out.println("BEFORE EACH");
-        time = LocalTime.now().getNano();
-        System.out.println(time);
+        calculator = new Calculator();
     }
 
     @AfterEach
     public void afterEach() {
         System.out.println("AFTER EACH");
-        time = LocalTime.now().getNano();
-        System.out.println(time);
     }
 
     @Test
     public void test() {
-        Calculator calculator = new Calculator();
-
+        //Calculator calculator = new Calculator();
         Integer sum = calculator.add(2, 2);
         Integer substract = calculator.substract(8, 4);
         Integer multiply = calculator.multiply(2, 2);
