4e311aa00b8b39570d2e96acd9dfba67611464f7
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-08-22 14:14:49 +0200

    refactored

--- src/test/java/c_param/CsvFileSourceTest.java
+++ src/test/java/c_param/CsvFileSourceTest.java
@@ -3,11 +3,10 @@ package c_param;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
-public class CsvSourceTest {
+public class CsvFileSourceTest {
     @ParameterizedTest
-    @CsvSource({"test,TEST", "Java,JAVA", "qwertyuiop, QWERTYUIOP"})
+    @CsvFileSource(resources = "/data.csv")
     public void test(String input, String expected) {
         Assertions.assertEquals(expected, input.toUpperCase());
     }
