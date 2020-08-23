109aee71af4d62c7314d0ba68d36bd56d56da517
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-08-23 11:18:30 +0200

    refactored

--- src/test/java/d_assertj/ZooTest.java
+++ src/test/java/d_assertj/ZooTest.java
@@ -1,12 +1,15 @@
 package d_assertj;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ZooTest {
     @Test
     public void zooTest() {
         Zoo zoo = new Zoo();
-        assertThat(zoo.getAllAnimals())
+        Animal cat = new Animal("Stefan");
+        assertThat(zoo.generateFunnyName(cat))
+                .isNotEqualTo(cat.getName());
     }
 }
