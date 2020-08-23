9e0703c4e4f3b6a554fbc24362f7188314c27cb9
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-08-23 11:32:29 +0200

    refactored

--- src/test/java/d_assertj/ZooTest.java
+++ src/test/java/d_assertj/ZooTest.java
@@ -2,9 +2,30 @@ package d_assertj;
 
 import org.junit.jupiter.api.Test;
 
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ZooTest {
+    @Test
+    public void newlyCreatedZooIsEmpty() {
+        Zoo zoo = new Zoo();
+        List<Animal> allAnimals = zoo.getAllAnimals();
+        assertThat(allAnimals)
+                .isEmpty();
+    }
+
+    @Test
+    public void zooHasOnlyOneAddedAnimal() {
+        Zoo zoo = new Zoo();
+        Animal animal = new Animal("cat");
+        zoo.addAnimal(animal);
+        List<Animal> allAnimals = zoo.getAllAnimals();
+        assertThat(allAnimals)
+                .hasSize(1);
+    }
+
     @Test
     public void zooTest() {
         Zoo zoo = new Zoo();
@@ -12,4 +33,5 @@ public class ZooTest {
         assertThat(zoo.generateFunnyName(cat))
                 .isNotEqualTo(cat.getName());
     }
+
 }
